a54226483666231aad1505e5a3f2500bd65bacbf
anakropusova1 <kropusova.nastya@mail.ru>
2026-09-15 17:24:32 +0300

    сделали ввод данных

---
+++ лр1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = ReadPositiveDouble("Введите длину комнаты (м): ");
+            double width = ReadPositiveDouble("Введите ширину комнаты (м): ");
+            double height = ReadPositiveDouble("Введите высоту комнаты (м): ");
+            double rollWidth = ReadPositiveDouble("Введите ширину рулона (м): ");
+            double rollLength = ReadPositiveDouble("Введите длину рулона (м): ");
+
+            int totalStrips = CalculateStrips(length, width, rollWidth);
+            int stripsPerRoll = CalculateStripsPerRoll(rollLength, height);
+            int rolls = CalculateRolls(totalStrips, stripsPerRoll);
+
+            Console.WriteLine("Количество полос: " + totalStrips);
+            Console.WriteLine("Количество рулонов: " + rolls);
+        }
+    }
+}
