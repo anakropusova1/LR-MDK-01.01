e2521f8e413dc5b3dea90e11401787ce311f60b0
anakropusova1 <kropusova.nastya@mail.ru>
2026-09-15 17:26:41 +0300

    ввод положительного числа с повторным запросом

---
+++ лр1/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        static double ReadPositiveDouble(string prompt)
+        {
+            double value;
+            while (true)
+            {
+                Console.Write(prompt);
+                string input = Console.ReadLine();
+
+                if (double.TryParse(input, out value) && value > 0)
+                    return value;
+
+                Console.WriteLine("Ошибка: введите положительное число. Повторите ввод.");
+            }
+        }
+    }
+}
